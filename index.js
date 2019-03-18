2a747a1c50d670113883cabc17c2db9917fdb9e7
Rudy Ravelin <rudy@noukod.com>
2019-03-18 16:37:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createPost() {
   let pageTemplate = _.template(document.getElementById('page-template').innerHTML);
   let postTemplate = _.template(document.getElementById('post-template').innerHTML);
   let commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
-  
+
   let title = document.getElementById("postTitle").value;
   let body = document.getElementById("postBody").value;
   let author = document.getElementById("postAuthor").value;
